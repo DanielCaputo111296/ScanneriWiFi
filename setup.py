e4ecb27d0b14b9e8ab8cc75cfb6d1d8104f1515c
DanielCaputo111296 <116673505+DanielCaputo111296@users.noreply.github.com>
2025-02-09 13:53:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-SSID_NAME = "Redmi Note 13 Pro 5G"
+SSID_NAME = "eero-wifi"
 
 # Scrive il valore SSID in un file config.py
 with open("scanner_wifi/config.py", "w") as f:
